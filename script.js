eec6ddbcbcd083c134dc8f91c4bcf632ab0eeead
Charles Loehle <clguitar1@gmail.com>
2021-08-02 10:24:37 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-const apikey = '6aa788cd5d3325df1ef11850cff774e7';
-
 const form = document.getElementById('form');
 const searchInput = document.getElementById('search');
 const main = document.getElementById('main');
 
-const url = location =>
-	`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=imperial`;
+const locationUrl = location => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=imperial`;
+const zipcodeUrl = zipcode => `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${apikey}&units=imperial`;
+
+form.addEventListener('submit', e => {
+	let alphabetRegex = /[a-z]/gi;
+	let digitRegex = /[0-9]/gi;
+
+	e.preventDefault();
+
+	const input = searchInput.value;
+
+	// check for mixed letters and numbers
+	if (alphabetRegex.test(input) && digitRegex.test(input)) {
+		console.log('Invalid input value');
+	} else if (alphabetRegex.test(input)) { // check for letters only
+		console.log(input);
+		if (input) {
+			getWeatherByLocation(input);
+		}
+	} else { // numbers only
+		console.log(input);
+		if (input) {
+			getWeatherByZipCode(input);
+		}
+	}
+});
 
 async function getWeatherByLocation(location) {
-	const res = await fetch(url(location), {
+	console.log('getWeatherByLocation called', location);
+	const res = await fetch(locationUrl(location), {
 		origin: 'cors',
 	});
+
+	const resData = await res.json();
+	console.log(resData);
+
+	addWeatherToPage(resData);
+}
+
+async function getWeatherByZipCode(zipcode) {
+	console.log('getWeatherByZipCode called', zipcode);
+	const res = await fetch(zipcodeUrl(zipcode), {
+		origin: 'cors',
+	});
+
 	const resData = await res.json();
 	console.log(resData);
 
@@ -24,7 +60,11 @@ function addWeatherToPage(data) {
 	const weather = document.createElement('div');
 	weather.classList.add('weather');
 
-	weather.innerHTML = `<h2>
+	// search by city 
+	weather.innerHTML = `
+		<h1>${data.sys.country}</h1>
+		<h1>${data.name}</h1>
+		<h2>
       <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />${temp}°F
       <small>${data.weather[0].main}</small>
     </h2>`;
@@ -35,27 +75,4 @@ function addWeatherToPage(data) {
 	main.appendChild(weather);
 }
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	// get search input value
-	// const city = searchInput.value;
 
-	// if (city) {
-	// 	getWeatherByLocation(city);
-	// }
-
-	const input = searchInput.value;
-
-	let alphabetRegex = /[a-z]/gi; // Change this line
-	let digitRegex = /[0-9]/gi;
-
-	if (alphabetRegex.test(input) && digitRegex.test(input)) {
-		// check for mixed letters and numbers
-		console.log('mixed letters and numbers');
-	} else if (alphabetRegex.test(input)) {
-		// check for letters only
-		console.log('letters');
-	} else {
-		console.log('numbers');
-	}
-});
