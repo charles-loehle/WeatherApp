22802b8eb46477abd6f79512f7593c387f8dbb2a
Charles Loehle <clguitar1@gmail.com>
2021-08-05 20:58:39 -0400

    another commit

--- script.js
+++ script.js
@@ -29,56 +29,83 @@ form.addEventListener('submit', e => {
 	}
 });
 
-async function useCurrentWeatherApi(zipcode) {
-	await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${apikey}&units=imperial&cnt=3`
+function useCurrentWeatherApi(zipcode) {
+	fetch(
+		`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${apikey}&units=imperial`
 	)
 		.then(res => res.json())
 		.then(data => {
-			showCurrentWeather(data)
-			useOneCallApi(data)
+			showCurrentWeather(data);
+			useOneCallApi(data);
 		});
 }
 
-function useOneCallApi(data){
-	const {lon, lat} = data.coord
-	console.log(data);
-	fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apikey}`).then(res => res.json()).then(data => {
-		showForecast([data])
-	})
+function useOneCallApi(data) {
+	const { lon, lat } = data.coord;
+	// console.log(data);
+	fetch(
+		`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${apikey}&units=imperial`
+	)
+		.then(res => res.json())
+		.then(data => {
+			showForecast([data]);
+		});
 }
 
 function showCurrentWeather(data) {
-	const {name} = data;
-	const {country} = data.sys;
-	const {description, icon} = data.weather[0]
-	const {temp, temp_min, temp_max} = data.main
+	const { name } = data;
+	const { country } = data.sys;
+	const { description, icon } = data.weather[0];
+	const { temp, temp_min, temp_max } = data.main;
+	const { speed } = data.wind;
 
-	console.log(data);
+	// console.log(data);
 
 	let now = new Date();
 
 	currentWeather.innerHTML = `
-	<h1>Current weather for ${name}, ${country}</h1>
 	<p>${dateBuilder(now)}</p>
-	<h2>
+	<h2>${name}, ${country}</h2>
+	<div class="current-temp">
 		<img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="weather" />
-		${Math.round(temp)}&#176
-		<small>${description}</small>
-	</h2>
+		<span>${Math.round(temp)}&#176</span>
+		<div class="description">${description}</div>
+		<div className="wind">Wind: ${speed}mph</div>
+	</div>
 	<h3>low ${Math.round(temp_min)}&#176;F - high ${Math.round(
 		temp_max
 	)}&#176;F</h3>	
 `;
-
-	forecastWeather.innerHTML = `
-
-		`;
 }
 
+// reference: https://javascript.plainenglish.io/display-7-day-weather-forecast-with-openweather-api-aac8ba21c9e3
 function showForecast(data) {
-	// console.log(data[0].daily);
-	data[0].daily.map(day => console.log(day.dt))
+	let forecastDay = '';
+	const dailyData = data[0].daily;
+	console.log(dailyData);
+
+	return dailyData.map((day, index) => {
+		// if (index > 0) {
+		let dayname = new Date(day.dt * 1000).toLocaleDateString('en', {
+			weekday: 'long',
+		});
+		const { description, icon } = day.weather[0];
+		const { min, max } = day.temp;
+
+		console.log(dayname);
+
+		forecastDay = `
+				<div className="forecastday">
+					<p>${dayname}</p>
+					<img src="https://openweathermap.org/img/wn/${icon}.png" alt="weather" />
+					<div className="forecastday__temp">${min} / ${max}&#176;F</div>
+					<span>${description}</span>
+				</div>
+			`;
+
+		forecastWeather.insertAdjacentHTML('afterbegin', forecastDay);
+		// }
+	});
 }
 
 function dateBuilder(d) {
